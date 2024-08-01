4e6140ea0f7e3f1f8dcb84d7da574a525119e6d3
Ming Soon Tee <tee_ming_soon@hotmail.com>
2024-08-01 19:50:38 +0800

    Update Mean Reversion.py

--- Mean Reversion.py
+++ Mean Reversion.py
@@ -59,10 +59,10 @@ def pnl(df, leverage):
     df["ratio"] = abs(df["Close_1"] * df["stock2_dailyReturns"]) / abs(df["Close"] * df["stock1_dailyReturns"])
     
     # Initialize new columns
-    df["fut ntl"] = 0.0
-    df["etf ntl"] = 0.0
-    df["fut cash"] = 0.0
-    df["etf cash"] = 0.0
+    df["stock1 ntl"] = 0.0
+    df["stock2 ntl"] = 0.0
+    df["stock1 cash"] = 0.0
+    df["stock2 cash"] = 0.0
     
     i = 30
     currFutNtl, currEtfNtl = 0, 0
@@ -73,28 +73,28 @@ def pnl(df, leverage):
             
             if abs(df.loc[i, 'stock1_dailyReturns']) > abs(df.loc[i, 'stock2_dailyReturns']):
                 if df.loc[i, 'stock1_dailyReturns'] > 0:
-                    df.loc[i, "fut ntl"] = df.loc[i, "ratio"] * -1 * df.loc[i, "leverage"]
-                    df.loc[i, "etf ntl"] = df.loc[i, "leverage"]
+                    df.loc[i, "stock1 ntl"] = df.loc[i, "ratio"] * -1 * df.loc[i, "leverage"]
+                    df.loc[i, "stock2 ntl"] = df.loc[i, "leverage"]
                 else:
-                    df.loc[i, "fut ntl"] = df.loc[i, "ratio"] * df.loc[i, "leverage"]
-                    df.loc[i, "etf ntl"] = -1 * df.loc[i, "leverage"]
+                    df.loc[i, "stock1 ntl"] = df.loc[i, "ratio"] * df.loc[i, "leverage"]
+                    df.loc[i, "stock2 ntl"] = -1 * df.loc[i, "leverage"]
             else:
                 if df.loc[i, 'stock2_dailyReturns'] > 0:
-                    df.loc[i, "fut ntl"] = df.loc[i, "ratio"] * df.loc[i, "leverage"]
-                    df.loc[i, "etf ntl"] = -1 * df.loc[i, "leverage"]
+                    df.loc[i, "stock1 ntl"] = df.loc[i, "ratio"] * df.loc[i, "leverage"]
+                    df.loc[i, "stock2 ntl"] = -1 * df.loc[i, "leverage"]
                 else:
-                    df.loc[i, "fut ntl"] = df.loc[i, "ratio"] * -1 * df.loc[i, "leverage"]
-                    df.loc[i, "etf ntl"] = df.loc[i, "leverage"]
+                    df.loc[i, "stock1 ntl"] = df.loc[i, "ratio"] * -1 * df.loc[i, "leverage"]
+                    df.loc[i, "stock2 ntl"] = df.loc[i, "leverage"]
                     
-            df.loc[i, "fut cash"] = df.loc[i, "fut ntl"] * df.loc[i, "Close"] * -1
-            df.loc[i, "etf cash"] = df.loc[i, "etf ntl"] * df.loc[i, "Close_1"] * -1
+            df.loc[i, "stock1 cash"] = df.loc[i, "stock1 ntl"] * df.loc[i, "Close"] * -1
+            df.loc[i, "stock2 cash"] = df.loc[i, "stock2 ntl"] * df.loc[i, "Close_1"] * -1
                     
-            currFutNtl = df.loc[i, "fut ntl"]
-            currEtfNtl = df.loc[i, "etf ntl"]
+            currFutNtl = df.loc[i, "stock1 ntl"]
+            currEtfNtl = df.loc[i, "stock2 ntl"]
 
         elif df.loc[i, "axe"] == -1:
-            df.loc[i, "fut cash"] = currFutNtl * df.loc[i, "Close"]
-            df.loc[i, "etf cash"] =  currEtfNtl * df.loc[i, "Close_1"]
+            df.loc[i, "stock1 cash"] = currFutNtl * df.loc[i, "Close"]
+            df.loc[i, "stock2 cash"] =  currEtfNtl * df.loc[i, "Close_1"]
             
             currFutNtl, currEtfNtl = 0, 0
     
@@ -149,11 +149,11 @@ def go(ticker1, ticker2, startDate, endDate, leverage):
     stock2["stock2_dailyReturns"] = stock2["Close"].pct_change().dropna()
 
     #stationary test
-    print("Test for stationary futures")
+    print("Test for stationary stock 1")
     stationaryTest(stock1["stock1_dailyReturns"].dropna())
-    print("Test for stationary etf")
+    print("Test for stationary stock 2")
     stationaryTest(stock2["stock2_dailyReturns"].dropna())
-    print("cointegration Test for stationary futures vs etf")
+    print("cointegration Test for stationary stock 1 vs stock 2")
     cointegrationTest(stock1["stock1_dailyReturns"].dropna(), stock2["stock2_dailyReturns"].dropna())
 
     combined_df = pd.concat([stock1, stock2], axis=1)
@@ -161,7 +161,7 @@ def go(ticker1, ticker2, startDate, endDate, leverage):
     combined_df["abs_sprd"] = abs(combined_df["spread"])
     combined_df["stock1_rolling avg"] = combined_df["stock1_dailyReturns"].rolling(window=30).std()
     combined_df["stock2_rolling avg"] = combined_df["stock2_dailyReturns"].rolling(window=30).std()
-
+    
     plt.figure(figsize=(12,6))
     plt.plot(combined_df["stock1_dailyReturns"], label="stock1 dailyReturns")
     plt.plot(combined_df["stock2_dailyReturns"], label="stock2 dailyReturns")
@@ -169,6 +169,7 @@ def go(ticker1, ticker2, startDate, endDate, leverage):
     plt.plot(combined_df["stock2_rolling avg"], label="stock2 rolling")
     plt.plot(combined_df["abs_sprd"], label="sprd", linestyle="--")
     plt.legend()
+    plt.grid(True)
     plt.show()
     
     df = signal(combined_df)
@@ -177,9 +178,9 @@ def go(ticker1, ticker2, startDate, endDate, leverage):
     
     print()
     
-    print("fut pnl = {}".format(df['fut cash'].sum()))
-    print("etf pnl = {}".format(df["etf cash"].sum()))
-    print("total pnl = {}".format(df["fut cash"].sum()+df["etf cash"].sum()))
+    print("stock1 pnl = {}".format(df['stock1 cash'].sum()))
+    print("stock2 pnl = {}".format(df["stock2 cash"].sum()))
+    print("total pnl = {}".format(df["stock1 cash"].sum()+df["stock2 cash"].sum()))
 
     df.to_csv("copper Fut VS ETF.csv", index=False)
 
